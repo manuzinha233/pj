d5955ede0f0b4157e388b31d90ac1603e78d1a69
Emanuely Silva <145017062+manuzinha233@users.noreply.github.com>
2025-03-28 08:06:15 -0300

    .

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-03-28T00:00:00");
 const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2025-03-31T00:00:00");
+const tempoObjetivo3 = new Date("2025-12-05T00:00:00");
 const tempoObjetivo4 = new Date("2025-12-05T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
